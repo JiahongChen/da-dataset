561fccfc2dd1657812bdd47bf115e9c23c9b1bf3
Jiahong Chen <15019313+JiahongChen@users.noreply.github.com>
2021-11-26 13:45:24 -0800

    Update segmentation.sh

--- scripts/segmentation.sh
+++ scripts/segmentation.sh
@@ -77,7 +77,7 @@ cd cityscapes
 
 wget --save-cookies cookies.txt \
      --keep-session-cookies \
-     --post-data 'username=${username}&password=${password}' \
+     --post-data 'username='${username}'&password='${password}'&submit=Login' \
      --delete-after \
      https://www.cityscapes-dataset.com/login/
 password = ''
