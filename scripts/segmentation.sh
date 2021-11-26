1f00853124d22519d5928203268a718170181f55
Jiahong Chen <15019313+JiahongChen@users.noreply.github.com>
2021-11-26 11:18:42 -0800

    Update segmentation.sh

--- scripts/segmentation.sh
+++ scripts/segmentation.sh
@@ -5,14 +5,15 @@ help()
 {
    echo "Usage: $0 -p [path-to-your-data-folder]"
    echo -e "\t-p: Path to your data folder"
+   echo -e "\t-u: Your Cityscape username"
    exit 1
 }
 
-while getopts "p:" opt
+while getopts "p:u:" opt
 do
    case "$opt" in
       p ) path="$OPTARG" ;;
-      # b ) param_b="$OPTARG" ;;
+      u ) username="$OPTARG" ;;
       ? ) help ;;
    esac
 done
@@ -25,9 +26,18 @@ then
    help
 fi
 
-# echo "$path"
+if [ -z "$username" ]
+then
+   echo "Your Cityscape username is empty";
+   help
+fi
 
-# GTA dataset
+read -s -p "Your Cityscape password: " password
+# echo "$path"
+# echo "$username"
+# echo "$password"
+
+GTA dataset
 cd "$path"
 mkdir gta
 cd gta
@@ -36,7 +46,11 @@ do
 	wget --no-check-certificate https://download.visinf.tu-darmstadt.de/data/from_games/data/${idx}_images.zip
 	wget --no-check-certificate https://download.visinf.tu-darmstadt.de/data/from_games/data/${idx}_labels.zip
 done
-unzip *.zip
+for idx in 01 02 03 04 05 06 07 08 09 10
+do
+	unzip ${idx}_images.zip
+	unzip ${idx}_labels.zip
+done
 
 #SYNTHIA
 cd "$path"
@@ -60,11 +74,13 @@ done
 cd "$path"
 mkdir cityscapes
 cd cityscapes
+
 wget --save-cookies cookies.txt \
      --keep-session-cookies \
-     --post-data 'username=your-user-name&password=your-password' \
+     --post-data 'username=${username}&password=${password}' \
      --delete-after \
      https://www.cityscapes-dataset.com/login/
+password = ''
 
 wget --load-cookies cookies.txt \
      https://www.cityscapes-dataset.com/file-handling/?packageID=3
